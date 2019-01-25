4daa42b1875b411bbb77a31328c58f0c9b5f3e32
Nick Fish <thenickfish@gmail.com>
2019-01-24 20:15:27 -0700

    improved clone all repo script

--- scripts/clone-all-repos.ps1
+++ scripts/clone-all-repos.ps1
@@ -1,12 +1,6 @@
-$bashScript=@'
-PAGE=1
-curl "https://api.github.com/users/thenickfish/repos?page=$PAGE&per_page=100" |
-  grep -e 'git_url*' |
-  cut -d \" -f 4 |
-  xargs -L1 git clone --depth=1
-'@
-
 mkdir C:\development
-Set-Location C:\development
-$bashScript | Out-File -Encoding ASCII -FilePath git-clone.sh
-cmd /c 'C:\Program Files\Git\bin\sh.exe' git-clone.sh
+set-location C:\development
+$repos = (invoke-webrequest -uri "https://api.github.com/users/thenickfish/repos?page=1&per_page=100" -UseBasicParsing).Content | convertfrom-json
+foreach ($repo in $repos){
+    git clone $repo.clone_url
+}
